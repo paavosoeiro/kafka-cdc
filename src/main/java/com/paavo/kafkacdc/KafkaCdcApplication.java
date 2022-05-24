d5dc5bbfc88ca14ba89877ceaab36f47f47a8575
paavosoeiro <paavomsoeiro@gmail.com>
2022-05-24 14:24:29 -0300

    Setup project

---
+++ src/main/java/com/paavo/kafkacdc/KafkaCdcApplication.java
@@ -0,0 +1,13 @@
+package com.paavo.kafkacdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paavo/kafkacdc/KafkaCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paavo.kafkacdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
